38341e02e456df8c61607e05bc1663bd07b0be55
David J. Brenes <david.brenes@simplelogica.net>
2014-07-08 09:44:07 +0200

    Adding friendly id to the job offer model

--- spec/models/no_cms/job_offers/job_offer_spec.rb
+++ spec/models/no_cms/job_offers/job_offer_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper.rb'
 
 describe NoCms::JobOffers::JobOffer do
   it_behaves_like "model with required attributes", :nocms_job_offers_job_offer, [:title, :description]
+  it_behaves_like "model with slug", :nocms_job_offers_job_offer, :title
 end
